8f907c048c7c1a9ed00df80411fc791c2cca90b9
raginikushwaha027 <raginikushwaha027@gmail.com>
2026-04-08 00:44:16 +0530

    Q1.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,6 +5,53 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
+      
+        for (int i = 0; i <= n; i++) {
+            
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+           
+            System.out.print("*");
+            
+          
+            if (i > 0 && i < n) {
+                for (int j = 0; j < 2 * i - 1; j++) {
+                    System.out.print(" ");
+                }
+            }
+            
+          
+            if (i > 0) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+        
+        
+        for (int i = n - 1; i >= 1; i--) {
+           
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            
+            System.out.print("*");
+            
+           
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print(" ");
+            }
+            
+            
+            System.out.print("*");
+            
+            System.out.println();
+        }
+    }
+}
+
         // TODO: Print a hollow diamond pattern of half-height n using '*'
         //
         // Rules:
@@ -21,5 +68,4 @@ public class Main {
         //   * *
         //    *
 
-    }
-}
+    
