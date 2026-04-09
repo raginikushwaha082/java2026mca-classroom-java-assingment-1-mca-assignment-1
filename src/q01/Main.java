8301b498e93d4da1e57c1bf62d7b6721d2fff06d
Ragini Kushwaha 083 <raginikushwaha027@gmail.com>
2026-04-09 10:47:56 +0530

    question 1 solved

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -5,67 +5,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-      
-        for (int i = 0; i <= n; i++) {
-            
-            for (int j = 0; j < n - i; j++) {
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // spaces before
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-           
-            System.out.print("*");
-            
-          
-            if (i > 0 && i < n) {
-                for (int j = 0; j < 2 * i - 1; j++) {
+
+            // hollow stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
                     System.out.print(" ");
                 }
             }
-            
-          
-            if (i > 0) {
-                System.out.print("*");
-            }
-            
+
             System.out.println();
         }
-        
-        
+
+        // Lower part
         for (int i = n - 1; i >= 1; i--) {
-           
-            for (int j = 0; j < n - i; j++) {
+
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-            
-            
-            System.out.print("*");
-            
-           
-            for (int j = 0; j < 2 * i - 1; j++) {
-                System.out.print(" ");
+
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                if (j == 1 || j == 2 * i - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
-            
-            
-            System.out.print("*");
-            
+
             System.out.println();
         }
     }
 }
-
-        // TODO: Print a hollow diamond pattern of half-height n using '*'
-        //
-        // Rules:
-        //   - Only border cells have '*', inside is spaces
-        //   - No trailing spaces on any line
-        //
-        // Input: 4
-        // Output:
-        //    *
-        //   * *
-        //  *   *
-        // *     *
-        //  *   *
-        //   * *
-        //    *
 
     
